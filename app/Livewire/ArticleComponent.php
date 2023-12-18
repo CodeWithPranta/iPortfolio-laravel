e0d00d05aff63212e1a08dd8607a44de1aa33fe2
webapp99 <codewithpranta@gmail.com>
2023-12-18 12:51:52 +0600

    latest article

--- app/Livewire/ArticleComponent.php
+++ app/Livewire/ArticleComponent.php
@@ -17,7 +17,7 @@ class ArticleComponent extends Component
     {
         if (!$this->search)
         {
-            $this->articles = Article::latest();
+            $this->articles = Article::latest()->get();
         }else
         {
             $this->articles = Article::where('title', 'like', '%' . $this->search . '%')
